766cfa4a60f828fb68b370a74c55094913084c9d
jardelbrando <jardelbrando@gmail.com>
2025-03-03 15:48:25 -0300

    Adding Difficulty Enumeration.

---
+++ src/main/java/com/example/recipeBook/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package com.example.recipeBook.domain;
+
+/**
+ * @author Jardel Brandao
+ * @date 03/03/2025
+ */
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/example/recipeBook/domain/Recipe.java
+++ src/main/java/com/example/recipeBook/domain/Recipe.java
@@ -32,6 +32,9 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     public Long getId() {
         return id;
     }
@@ -118,5 +121,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
