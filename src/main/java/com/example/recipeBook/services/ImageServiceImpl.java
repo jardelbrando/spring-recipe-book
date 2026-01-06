edeccf7caf3d7a5f2c4900d661810785c565be5d
Jardel <jardelbrando@gmail.com>
2026-01-06 16:42:38 -0300

    Showing image from databse

--- src/main/java/com/example/recipeBook/services/ImageServiceImpl.java
+++ src/main/java/com/example/recipeBook/services/ImageServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.recipeBook.domain.Recipe;
 import com.example.recipeBook.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,34 +17,37 @@ import java.io.IOException;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+
     private final RecipeRepository recipeRepository;
 
-    public ImageServiceImpl(RecipeRepository recipeService){
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
         this.recipeRepository = recipeService;
     }
 
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        try{
+        try {
             Recipe recipe = recipeRepository.findById(recipeId).get();
 
-            Byte[] byteObject = new Byte[file.getBytes().length];
+            Byte[] byteObjects = new Byte[file.getBytes().length];
 
             int i = 0;
 
-            for(byte b : file.getBytes()){
-                byteObject[i++] = b;
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
             }
 
-            recipe.setImage(byteObject);
+            recipe.setImage(byteObjects);
 
             recipeRepository.save(recipe);
-        }catch (IOException e){
-            log.error("Error occurred when saving image", e);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
 
             e.printStackTrace();
         }
-
     }
 }
