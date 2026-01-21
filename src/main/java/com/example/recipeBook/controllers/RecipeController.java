ba8956eecc6f46cd37ba51533fc54d2545004921
Jardel <jardelbrando@gmail.com>
2026-01-21 14:29:42 -0300

    displaying validation error with thymeleaf

--- src/main/java/com/example/recipeBook/controllers/RecipeController.java
+++ src/main/java/com/example/recipeBook/controllers/RecipeController.java
@@ -8,10 +8,12 @@ package com.example.recipeBook.controllers;
 import com.example.recipeBook.commands.RecipeCommand;
 import com.example.recipeBook.exceptions.NotFoundException;
 import com.example.recipeBook.services.RecipeService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -46,8 +48,15 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+            return "recipe/recipeForm";
+        }
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
